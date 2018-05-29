44c699ffab8f8c3fb3884d916e272ca527217093
[Mark Bilginer] <[markbilginer@gmail.com]>
2018-05-30 00:18:12 +0300

    finished project:extended layout.scss; modified modules.scss, modified config.rb -> changed to :expanded

--- config.rb
+++ config.rb
@@ -3,5 +3,6 @@ require 'susy'
 css_dir = '_/css'
 sass_dir = '_/components/sass'
 javascripts_dir = '_/js'
-output_style = :compressed
+# change this to :compressed to minify css, before production
+output_style = :expanded
 relative_assets= true;
